095d2c676f8133de534feaf9061854a07c7904d5
Scandula <scandula@protonmail.ch>
2019-07-31 17:53:01 +0100

    moderation(ban): initial commit

---
+++ commands/moderation/ban.js
@@ -0,0 +1,37 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class Ban extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'ban',
+            aliases: ['b', 'bean'],
+            group: 'moderation',
+            memberName: 'ban',
+            description: 'Bans a user from the guild.',
+            userPermissions: ['BAN_MEMBERS'],
+            clientPermissions: ['BAN_MEMBERS'],
+            args: [
+                {
+                    key: 'member',
+                    prompt: 'Which member would you like to ban?',
+                    type: 'member'
+                },
+                {
+                    key: 'reason',
+                    prompt: 'Why would you like to ban this member?',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+
+    run(message, { member, reason }) {
+        try {
+            // member.ban(reason);
+        }
+        catch(e) {
+            console.log(e.toString());
+        }
+        return message.reply(`successfully banned ${member.user.username} for ${reason}`);
+    }
+};
