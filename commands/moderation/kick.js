13995bd5321fdc4c0f8724b080184489ff6eba0e
Scandula <scandula@protonmail.ch>
2019-07-31 17:28:45 +0100

    moderation(kick): initial commit

---
+++ commands/moderation/kick.js
@@ -0,0 +1,37 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class Kick extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'kick',
+            aliases: ['k'],
+            group: 'moderation',
+            memberName: 'kick',
+            description: 'Kicks a user from the guild.',
+            userPermissions: ['SEND_MESSAGES'],
+            clientPermissions: ['KICK_MEMBERS'],
+            args: [
+                {
+                    key: 'member',
+                    prompt: 'Which member would you like to kick?',
+                    type: 'member'
+                },
+                {
+                    key: 'reason',
+                    prompt: 'Why would you like to kick this member?',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+
+    run(message, { member, reason }) {
+        try {
+            member.kick(reason);
+        }
+        catch (e) {
+            console.log(e.toString());
+        }
+        return message.reply(`successfully kicked ${member.user.username} for ${reason}`);
+    }
+};
