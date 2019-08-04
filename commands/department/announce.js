5b0b99a3bc1e3a12281b47a8ee89bd36d1a229e2
Scandula <scandula@protonmail.ch>
2019-08-05 00:34:39 +0100

    feat(department): add a new announcement command

---
+++ commands/department/announce.js
@@ -0,0 +1,52 @@
+const { Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+
+module.exports = class Announce extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'announce',
+            aliases: [
+                'lspd-announce',
+                'lssd-announce',
+                'sacd-announce',
+                'lsfd-announce',
+                'sahp-announce',
+                'vm-announce'
+            ],
+            group: 'department',
+            memberName: 'announce',
+            description: 'Sends an announcement to the channel.',
+            userPermissions: ['MANAGE_MESSAGES'],
+            clientPermissions: ['EMBED_LINKS'],
+            guildOnly: true,
+            hidden: true,
+            args: [
+                {
+                    key: 'announcement',
+                    prompt: 'What would you like to announce?',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+
+    run(message, { announcement }) {
+        const deliminator = announcement.split(/title: (.+) \|/);
+
+        message.delete();
+
+        const embed = new RichEmbed()
+            .setAuthor(`Announcement from ${message.member.user.username}`, message.author.avatarURL)
+            .setDescription(announcement)
+            .setColor('#1C98F8')
+            .setFooter(message.member.highestRole.name)
+            .setTimestamp();
+
+        if (deliminator[1] !== undefined) {
+            embed.setTitle(deliminator[1].replace('title: ', ''));
+            embed.setDescription(announcement.replace(`title: ${deliminator[1]} |`, ''));
+        }
+
+        message.say(embed);
+    }
+};
