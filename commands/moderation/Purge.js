dae379f341675b4d0c0824268efbe461089e8263
Scandula <scandula@protonmail.ch>
2019-08-15 23:06:38 +0100

    feat(purge): bulk delete messages

---
+++ commands/moderation/Purge.js
@@ -0,0 +1,33 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class Purge extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'purge',
+            aliases: ['prune', 'bulkdel'],
+            group: 'moderation',
+            memberName: 'purge',
+            description: 'Bulk deletes a specific amount of messages.',
+            userPermissions: ['MANAGE_MESSAGES'],
+            clientPermissions: ['MANAGE_MESSAGES'],
+            guildOnly: true,
+            hidden: true,
+            args: [
+                {
+                    key: 'amount',
+                    prompt: 'How many messages would you like to delete? **(Max 100)**',
+                    type: 'integer',
+                    validate: int => int < 101 && int > 0
+                }
+            ]
+        });
+    }
+
+    run(message, { amount }) {
+        message.delete();
+
+        message.channel.bulkDelete(amount)
+            .then(bulkDeleted => message.reply(`I deleted ${bulkDeleted.size} message${bulkDeleted.size > 1 ? 's' : ''} for you.`))
+            .catch(err => message.reply('Uh oh, something went wrong. Developers notified.') & console.log(err.stack));
+    }
+};
