7d31a3a51fcc1c13f570805ef513a2d7e47d9552
Max Fury <xTornadoZz@outlook.com>
2019-08-20 00:02:04 +0300

    dev(lockdown): text channels compatibility

--- commands/administration/lockdown.js
+++ commands/administration/lockdown.js
@@ -6,64 +6,66 @@ module.exports = class lockdown extends Command {
     constructor(client) {
         super(client, {
             name: 'lockdown',
-            aliases: ['lockd', 'ld'],
+            aliases: ['lock', 'ld'],
             group: 'administration',
             memberName: 'lockdown',
-            description: 'Locks all channels in a specific category.',
-            userPermissions: ['MANAGE_GUILD'],
+            description: 'Locks a text channel or all channel of a category.',
+            userPermissions: ['MANAGE_CHANNELS'],
             clientPermissions: ['MANAGE_CHANNELS'],
-            guildOnly: true,
-            hidden: true,
+            guildOnly: null,
+            hidden: null,
             args: [
                 {
-                    key: 'category',
-                    prompt: 'Which category do you want to lock?',
+                    key: 'channel',
+                    prompt: 'Which channel do you want to lock?',
                     type: 'channel'
                 },
                 {
                     key: 'reason',
                     prompt: 'Why are you locking?',
                     type: 'string',
-                    default: ''
-                },
-                {
-                    key: 'end',
-                    prompt: 'Stop this lockdown?',
-                    type: 'boolean',
-                    default: false
+                    default:'No reason provided!'
                 }
             ]
         });
     }
 
-    run(message, { category, reason, end }) {
-        message.delete();
-
-        if (category.type !== 'category') {
-            return message.reply('you need to provide a category to lock.');
-        }
-
-        const children = category.children;
-
-        children.forEach(child => {
-            child.overwritePermissions(message.guild.defaultRole, {
-                SEND_MESSAGES: end
+    run(message, { channel, reason }) {
+        const _role = message.guild.roles.get(message.guild.id);
+        if (channel.type === 'category') {
+            const children = channel.children;
+            children.forEach(child => {
+                if(child.type === 'voice') return;
+                child.overwritePermissions(_role, {
+                    SEND_MESSAGES: false
+                });
+                console.log('Locked channel [ ' + child.name + ' ].');
+                child.send('🔒 This channel was locked by ' + message.member.displayName);
             });
-        });
-
-        const embed = new RichEmbed()
-            .setAuthor('Lockdown | ' + message.author.tag, message.author.avatarURL)
-            .setDescription(`Category ${category.name} has been ${end ? 'unlocked' : 'locked'}.`)
-            .setColor(config.embedColors.action)
-            .setTimestamp();
-
-        if (reason && reason !== '') {
-            embed.addField('Reason', reason);
+            const embed = new RichEmbed()
+                .setAuthor('Lockdown Start | ' + message.author.tag, message.author.avatarURL)
+                .setDescription(`Category ${channel.name} has been locked.`)
+                .setColor(config.embedColors.action)
+                .addField('Reason', reason)
+                .setTimestamp();
+            this.client.channels.find(logC => logC.id === config.logChannels.channel).send({ embed: embed });
         }
-
-
-        this.client.channels.find(logC => logC.id === config.logChannels.actions).send({ embed: embed });
-        message.reply(`I ${end ? 'unlocked' : 'locked'} the \`${category.name}\` category for you.`);
-
+        else if (channel.type === 'text') {
+            channel.overwritePermissions(_role, {
+                SEND_MESSAGES: false
+            });
+            console.log('Locked channel [ ' + channel.name + ' ].');
+            const embed = new RichEmbed()
+                .setAuthor('Lockdown  | ' + message.author.tag, message.author.avatarURL)
+                .setDescription(`Channel ${channel.name} has been locked.`)
+                .setColor(config.embedColors.action)
+                .addField('Reason', reason)
+                .setTimestamp();
+            this.client.channels.find(logC => logC.id === config.logChannels.channel).send({ embed: embed });
+            return message.guild.channels.get(channel.id).send('🔒 This channel was locked by ' + message.member.displayName);
+        }
+        else {
+            return message.reply('you need to provide a text channel or a category to lock.');
+        }
     }
 };
