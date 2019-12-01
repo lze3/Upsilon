67e67489c69d159e876ec3c18098d37ff2530397
Zeemah <jhodgson1@protonmail.ch>
2019-12-01 19:59:43 +0000

    utils(status): working version

--- src/bot/utils/server-status-tracking.ts
+++ src/bot/utils/server-status-tracking.ts
@@ -1,7 +1,9 @@
-import { Channel, TextChannel, GuildChannel, Guild } from 'discord.js';
+import { Channel, TextChannel, GuildChannel, Guild, MessageEmbed, EmbedField, Message, DiscordAPIError } from 'discord.js';
 import { client } from '../bot';
 import { ArgumentType } from 'discord.js-commando';
 import * as request from 'request';
+import * as moment from 'moment';
+import { SpawnSyncOptionsWithStringEncoding } from 'child_process';
 
 const settings: {logStatus: boolean, statusChannels: string[], waitTime: number} = {
     /**
@@ -22,7 +24,7 @@ const settings: {logStatus: boolean, statusChannels: string[], waitTime: number}
     waitTime: 7500
 }
 
-const hsgAuths = {
+const hsgAuths: any = {
     'CR': 'Casual Restricted',
     'CU': 'Casual Unrestricted',
     'M1': 'New Member',
@@ -53,14 +55,12 @@ function getServerInfoData(): void {
     if (!settings.statusChannels || typeof settings.statusChannels !== 'object' || settings.statusChannels.length === 0) return;
 
     // iteration
-    for (const channel of Object.keys(settings.statusChannels)) {
+    for (const channel of settings.statusChannels) {
         
-        let guildChannel: TextChannel|undefined;
+        let guildChannel: TextChannel;
 
         // get channel from client's channel collection
-        client.guilds.forEach(guild => {
-            guildChannel = (guild.channels.get(channel) as TextChannel);
-        });
+        guildChannel = client.channels.find(ch => ch.id === channel) as TextChannel;
 
         // if channel couldn't be found in collection, return 
         if (guildChannel === undefined) return console.log('Could not find channel (%s) in bot\'s collection.', channel);
@@ -117,13 +117,11 @@ function setServerStatusInfoThread(): void {
     // if no channels then no endpoints
     if (!settings.statusChannels || typeof settings.statusChannels !== 'object' || settings.statusChannels.length === 0) return;
 
-    for (const channel of Object.keys(settings.statusChannels)) {
+    for (const channel of settings.statusChannels) {
     
         let guildChannel: TextChannel|undefined;
 
-        client.guilds.forEach(guild => {
-            guildChannel = (guild.channels.get(channel) as TextChannel);
-        });
+        guildChannel = client.channels.find(ch => ch.id === channel) as TextChannel;
 
         // if the channel doesn't exist in the client's collection, we stop the code
         if (guildChannel === undefined) return console.log('Could not find channel (%s) in bot\'s collection.', channel);;
@@ -153,11 +151,11 @@ function setServerStatusInfoThread(): void {
             }
         });
 
-        if (playerData[channel] === undefined) return console.log('playerData\'%s\'] was undefined, running again...', channel);
+        if (playerData[channel] === undefined) return console.log('playerData[\'%s\'] was undefined, running again...', channel);
 
-        if (serverData[channel] === undefined) return console.log('serverData\'%s\'] was undefined, running again...', channel);
+        if (serverData[channel] === undefined) return console.log('serverData[\'%s\'] was undefined, running again...', channel);
 
-        if (serverData[channel].Data === undefined) return console.log('serverData\'%s\'].Data was undefined, running again...', channel);
+        if (serverData[channel].Data === undefined) return console.log('serverData[\'%s\'].Data was undefined, running again...', channel);
 
         if (serverData[channel].state !== undefined) isProbablyOffline = true;
 
@@ -165,22 +163,142 @@ function setServerStatusInfoThread(): void {
             '`' + playerData[channel].map((ply: playerDataStruct) => `${ply.name}`).join(', ') + '`' :
             'No players online.';
 
-        let additionalFields: object[];
-        const shortAlvl = hsgAuths[serverData[channel].Data.gametype.replace('HSG-RP | Authorization ', '')] !== undefined ?? null;
-        if (!isProbablyOffline) {
-            additionalFields = [
-                {
-                    name: 'Authorization',
-                    value: hsgAuths[shortAlvl] + ` (${shortAlvl})`
-                }
-            ]
+        let additionalFields: EmbedField[];
+        const emptyFields: EmbedField[] = [];
+        if (serverData[channel].Data.gametype.includes('Authorization')) {
+            const shortAlvl = hsgAuths[serverData[channel].Data.gametype.replace('HSG-RP | Authorization ', '')];
+            if (!isProbablyOffline) {
+                additionalFields = [
+                    {
+                        name: 'Authorization',
+                        value: hsgAuths[shortAlvl] + ` (${shortAlvl})`
+                    },
+                    {
+                        name: 'Roleplay Zone',
+                        value: serverData[channel].Data.mapname
+                    }
+                ]
+            }
         }
+
+        guildChannel.messages.fetch()
+            .then(messages => {
+                let statEmbed;
+                let offlineEmbed;
+                if (!isProbablyOffline) {
+                    statEmbed = new MessageEmbed()
+                        .setColor('#7700EF')
+                        .setAuthor('HighSpeed-Gaming', 'https://i.imgur.com/qTPd0ql.png')
+                        .setTitle('Here is the updated server status, last updated @ ' + moment(Date.now()).format('h:mm:ss') + '\n\n' +
+                            `Total players: ${playerData[channel].length}/${serverData[channel].Data.vars.sv_maxClients}`)
+                        .setDescription(format)
+                        .setFooter('HighSpeed-Gaming 2019');
+
+                    if (typeof additionalFields === 'object') {
+                        statEmbed.fields = additionalFields;
+                    }
+                }
+                else {
+                    offlineEmbed = new MessageEmbed()
+                        .setColor('#7700EF')
+                        .setAuthor('HighSpeed-Gaming', 'https://i.imgur.com/qTPd0ql.png')
+                        .setTitle('Server Offline! Last updated @ ' + moment(Date.now()).format('h:mm:ss'))
+                        .setFooter('HighSpeed-Gaming 2019');
+                }
+
+                if (messages.array().length === 0) {
+                    console.log('There were no messages in the channel (%s), so I am sending the initial embed now...', guildChannel?.name);
+                    if (isProbablyOffline) {
+                        console.log('I think the server is offline.');
+                        return guildChannel?.send(offlineEmbed);
+                    }
+                    
+                    return guildChannel?.send(statEmbed);
+                }
+
+                messages.forEach(message_ => {
+                    if (message_ === null) return console.log('I found a null message object, running again.');
+
+                    if (message_.author.id !== client.user?.id) { return message_.delete(); }
+
+                    if (message_.embeds.length >= 1) {
+                        console.log('I found a message (%s) in the channel (%s) with embeds, editing this message with the updated information.',
+                            message_.id,
+                            guildChannel?.name);
+
+                        if (!isProbablyOffline) {
+                            const embed = new MessageEmbed(message_.embeds[0])
+                                .setTitle('Server Offline! Last updated @ ' + moment(Date.now()).format('h:mm:ss'));
+
+                            delete embed.fields;
+                            delete embed.description;
+
+                            return message_.edit(embed);
+                        }
+
+                        const embed = new MessageEmbed()
+                            .setDescription(format)
+                            .setTitle('Here is the updated server status, last updated @ ' + moment(Date.now()).format('h:mm:ss') + '\n\n' +
+                                `Total players: ${playerData[channel].length}/${serverData[channel].Data.vars.sv_maxClients}`);
+                            
+                        embed.fields = additionalFields;
+
+                        return message_.edit(embed);
+                    }
+                    else {
+                        message_.delete();
+                        console.log('I found a message in %s by %s that was not status in #%s (%s)',
+                            guildChannel?.name,
+                            message_.author.tag,
+                            guildChannel?.name,
+                            guildChannel?.id);
+                    }
+                });
+            });
     }
 }
+const setServerInfoThread = setInterval(setServerStatusInfoThread, settings.waitTime || 3000);
 
 interface playerDataStruct {
     name: string;
     id: number;
-    identifiers: object;
+    identifiers: string[];
     ping: number;
 }
+
+/*
+interface serverDataStruct {
+    [key: string]: any;
+    Data: {
+        sv_maxClients: number;
+        clients: number;
+        gamename: string;
+        protocol: number;
+        hostname: string;
+        gametype: string;
+        mapname: string;
+        enhancedHostSupport: boolean;
+        resources: string[];
+        server: string;
+        vars: {
+            Teamspeak?: string;
+            Website?: string;
+            locale?: string;
+            onesync_enabled: string;
+            sv_enhancedHostSupport: string;
+            sv_lan: string;
+            sv_licenseKeyToken: string;
+            sv_maxClients: string;
+            sv_scriptHookAllowed: string;
+            svn?: string;
+            tags: string;
+            premium?: string;
+        };
+        players: object[];
+        upvotePower: number;
+        svMaxclients: number;
+        lastSeen: Date;
+        iconVersion: number;
+    }
+}
+*/
